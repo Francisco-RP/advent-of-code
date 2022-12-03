248b1330bd7acafd545658cc66a94c7f71e454dc
Francisco-RP <80427096+Francisco-RP@users.noreply.github.com>
2022-12-03 01:47:24 -0500

    day-3 solved

---
+++ day-3/index.js
@@ -0,0 +1,69 @@
+import assert from "node:assert/strict";
+import "../lib.js";
+import { testInput, input } from "./input.js";
+
+// https://adventofcode.com/2022/day/3
+
+function toInt(char) {
+  if (/[a-z]/.test(char)) {
+    return char.charCodeAt(0) - 96;
+  }
+  return char.charCodeAt(0) - 38;
+}
+
+/**
+ * @param {string} input
+ * @returns {number}
+ */
+function findItems(input) {
+  return input
+    .trim()
+    .split("\n")
+    .map((sack) => {
+      const half = sack.length / 2;
+      const left = sack.substring(0, half);
+      const right = sack.substring(half);
+      const sharedType = left
+        .split("")
+        .filter((char) => right.includes(char))
+        .dedupe()
+        .pop();
+      return toInt(sharedType);
+    })
+    .sum();
+}
+
+// test
+assert.equal(findItems(testInput), 157);
+
+const resultPart1 = findItems(input);
+assert.equal(resultPart1, 7716);
+console.log("Result 1:", resultPart1);
+
+/*********************************
+ * Part 2
+ */
+
+/**
+ * Search group of 3 rucksacks for same letter (badge)
+ * @param {string[]} group
+ * @returns {string}
+ */
+function searchGroup(group) {
+  return group[0]
+    .split("")
+    .filter((char) => group[1].includes(char) && group[2].includes(char))
+    .dedupe()
+    .pop();
+}
+
+function findBadges(input) {
+  return input.trim().split("\n").chunk(3).map(searchGroup).map(toInt).sum();
+}
+
+// test
+assert.equal(findBadges(testInput), 70);
+
+const resultPart2 = findBadges(input);
+assert.equal(resultPart2, 2973);
+console.log("Result 2:", resultPart2);
--- lib.js
+++ lib.js
@@ -1,3 +1,33 @@
 Array.prototype.sum = function () {
   return this.reduce((a, b) => a + b, 0);
 };
+
+Array.prototype.dedupe = function () {
+  const arr = [];
+  this.forEach((item) => {
+    if (!arr.includes(item)) {
+      arr.push(item);
+    }
+  });
+  return arr;
+};
+
+/**
+ * Take a flat array and split it into chunks of `size` length
+ * @param {unknown[]} size size of each chunk
+ * @returns {unknown[][]} an array of chunks (which are also arrays)
+ */
+Array.prototype.chunk = function (size) {
+  let chunk = [];
+  const chunks = [];
+  this.forEach((item) => {
+    if (chunk.length < size) {
+      chunk.push(item);
+    } else {
+      chunks.push(chunk);
+      chunk = [item];
+    }
+  });
+  if (chunk.length) chunks.push(chunk);
+  return chunks;
+};
