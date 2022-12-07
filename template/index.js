30ac8068e03e02c353638b3d1ed1bd25841d3c3f
Francisco-RP <80427096+Francisco-RP@users.noreply.github.com>
2022-12-07 11:28:40 -0500

    update template yet again

--- template/index.js
+++ template/index.js
@@ -1,8 +1,20 @@
 import assert from "node:assert/strict";
 import fs from "node:fs";
 
+/***********************************************************************
+ * Input data
+ */
+
 const input = fs.readFileSync("./input.txt", { encoding: "utf-8" });
 const testInput = ``;
+
+/***********************************************************************
+ * Shared functions/classes
+ */
+
+/***********************************************************************
+ * Part 1
+ */
 
 /**
  * @param {string} str the input string
@@ -11,18 +23,17 @@ const testInput = ``;
 function part1(str) {}
 
 // test
-// assert.equal(part1(testInput), ?????);
+// assert.equal(part1(testInput), AAAAAA);
 
 // console.time('Part 1');
 // const result1 = part1(input);
 // console.timeEnd('Part 1');
 
-// when I correctly solve the problem, I like to add this assertion so that if I refactor I know if I've broken anything
-// assert.equal(result1, ????);
+// assert.equal(result1, AAAAAA);
 
 // console.log("Result 1:", result1);
 
-/*********************************
+/***********************************************************************
  * Part 2
  */
 
@@ -33,13 +44,12 @@ function part1(str) {}
 function part2(str) {}
 
 // test
-// assert.equal(part2(testInput), ?????);
+// assert.equal(part2(testInput), AAAAAA);
 
 // console.time('Part 2');
 // const result2 = part2(input);
 // console.timeEnd('Part 2');
 
-// when I correctly solve the problem, I like to add this assertion so that if I refactor I know if I've broken anything
-// assert.equal(result2, ????);
+// assert.equal(result2, AAAAAA);
 
 // console.log("Result 2:", result2);
