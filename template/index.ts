fbe3bf538ee3281fee1c7e1c086e3a1d7cec92b0
Francisco-RP <80427096+Francisco-RP@users.noreply.github.com>
2023-12-01 14:35:26 -0500

    got debugging working, updated template

--- template/index.ts
+++ template/index.ts
@@ -1,4 +1,12 @@
-const input = await Deno.readTextFile("./input.txt");
+const __dirname = new URL(".", import.meta.url).pathname;
+
+let input: string;
+if (Deno.env.get("DEBUGGING")) {
+  // change the input specifically for debugging
+  input = await Deno.readTextFile(__dirname + "/input.txt");
+} else {
+  input = await Deno.readTextFile(__dirname + "/input.txt");
+}
 
 /***********************************************************************
  * Part 1
