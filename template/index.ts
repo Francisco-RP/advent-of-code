d3330c24bd5c3937b7a63ba447c5fb4fdc45729a
Francisco-RP <80427096+Francisco-RP@users.noreply.github.com>
2023-12-01 12:27:05 -0500

    updated the deno template

---
+++ template/index.ts
@@ -0,0 +1,22 @@
+const input = await Deno.readTextFile("./input.txt");
+
+/***********************************************************************
+ * Part 1
+ */
+
+export function part1(str: string): number {
+  return 0;
+}
+
+/***********************************************************************
+ * Part 2
+ */
+
+export function part2(str: string): number {
+  return 0;
+}
+
+if (!Deno.env.get("TESTING")) {
+  console.log(part1(input));
+  console.log(part2(input));
+}
