ac77a4392768cce543338b41f00da77ece6b5046
Francisco-RP <80427096+Francisco-RP@users.noreply.github.com>
2023-12-06 00:30:51 -0500

    solved day-6 part 1

---
+++ 2023/day-6/index.ts
@@ -0,0 +1,79 @@
+const __dirname = new URL(".", import.meta.url).pathname;
+
+let input: string;
+if (Deno.env.get("DEBUGGING")) {
+  // change the input specifically for debugging
+  input = await Deno.readTextFile(__dirname + "/input.txt");
+} else {
+  input = await Deno.readTextFile(__dirname + "/input.txt");
+}
+
+input = input.trim();
+
+interface Race {
+  /**
+   * time allowed for each race in milliseconds
+   */
+  time: number;
+  /**
+   * best distance ever recorded in that race in millimeters
+   */
+  record: number;
+}
+
+function setup(str: string): Race[] {
+  const races: Race[] = [];
+  const [timeLine, distanceLine] = str.split(/\n/);
+  const time = timeLine.match(/\d+/g)?.map(Number) || [];
+  const distance = distanceLine.match(/\d+/g)?.map(Number) || [];
+  for (let i = 0; i < time.length; i++) {
+    races.push({
+      time: time[i],
+      record: distance[i],
+    });
+  }
+  return races;
+}
+
+/***********************************************************************
+ * Part 1
+ */
+
+/**
+ * find out how many ways to you can win the race
+ */
+function checkRace({ time, record }: Race): number {
+  // I need to beat race.record within race.time
+  // I start off at speed 0;
+  // for every ms I hold, I gain 1mm/s
+  // but I also waste 1ms of the race.time
+  let ways = 0;
+  let speed = 0;
+  for (let i = 1; i <= time; i++) {
+    speed += 1;
+    const remainingTime = time - i;
+    const dist = remainingTime * speed;
+    if (dist > record) ways += 1;
+  }
+
+  return ways;
+}
+
+export function part1(str: string): number {
+  const races = setup(str);
+  const product = races.map(checkRace).reduce((prod, n) => prod * n, 1);
+  return product;
+}
+
+/***********************************************************************
+ * Part 2
+ */
+
+export function part2(str: string): number {
+  return 0;
+}
+
+if (!Deno.env.get("TESTING")) {
+  console.log(part1(input));
+  console.log(part2(input));
+}
---
+++ 2023/day-6/test.ts
@@ -0,0 +1,27 @@
+import { assertStrictEquals } from "std/assert/mod.ts";
+import { part1 } from "./index.ts";
+import { part2 } from "./index.ts";
+
+Deno.env.set("TESTING", "true");
+
+Deno.test("part 1 example input", () => {
+  const testInput = `
+Time:      7  15   30
+Distance:  9  40  200
+`.trim();
+  assertStrictEquals(part1(testInput), 288);
+});
+
+// Deno.test("Part 1 still produces the accepted answer", async () => {
+//   const input = await Deno.readTextFile("./input.txt");
+//   assertStrictEquals(part1(input), 111111111111111);
+// });
+
+// Deno.test("part 2 example input", () => {
+//   assertStrictEquals(part2(testInput), 1111111111111);
+// });
+
+// Deno.test("Part 2 still produces the accepted answer", async () => {
+//   const input = await Deno.readTextFile("./input.txt");
+//   assertStrictEquals(part2(input), 111111111111111);
+// });
